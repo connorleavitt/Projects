0eef0a0b42fd2f90654231ca9ed636aabe2d70b2
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-08-10 17:36:45 -0700

    Add Sketch changes

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,81 @@
+const DEFAULT_PEN_COLOR = '#6a0dad';
+const DEFAULT_MODE = 'color';
+const DEFAULT_SIZE = 16;
+
+let currentPenColor = DEFAULT_PEN_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
+let currentlyActive = false;
+
+function setCurrentPenColor(newPenColor) {
+    currentPenColor = newPenColor;
+  }
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+  }
+
+
+
 const container = document.querySelector('#container');
+const userGridSize = document.querySelector('#grid-size');
+const clearGridBtn = document.querySelector('#clear-grid');
+const penColorChoice = document.getElementById("penColor");
+const clearButton = document.getElementById("clear-grid");
+const cell = document.getElementsByClassName("grid-item");
+
+userGridSize.onmousemove = (e) => gridSizeValue(e.target.value)
+userGridSize.onchange = (e) => updateSize(e.target.value)
+penColorChoice.oninput = (e) => setCurrentPenColor(e.target.value)
+clearButton.onclick = () => resetGrid()
+
+function gridSizeValue(value) {
+    document.getElementById("gridSizeValue").innerHTML = `${value} x ${value}`;
+}
+
+function updateSize(value) {
+    setCurrentSize(value)
+    gridSizeValue(value)
+    resetGrid(value)
+}
+
+function resetGrid() {
+    clearGrid();
+    generateGrid(currentSize);
+}
+
+function clearGrid() {
+    container.innerHTML = ''
+    // container.replaceChildren();
+  }
 
 
-function generateGrid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (let i = 0; i < (rows * cols); i++) {
+function generateGrid(size) {
+    clearGrid()
+    container.style.setProperty('--grid-rows', size);
+    container.style.setProperty('--grid-cols', size);
+    for (let i = 0; i < (size * size); i++) {
         let newGridDiv = document.createElement('div');
-        newGridDiv.classList.add("grid");
+        newGridDiv.classList.add("grid-item");
         container.append(newGridDiv);
     }
 }
-generateGrid(2,2);
 
+function togglePen() {
+    if (!currentlyActive) {
+        cell.forEach( e => {
+            e.addEventListener('mouseleave', activatePen)
+        })
+    }
+}
+
+container.addEventListener('mousedown', () => togglePen());
+
+function activatePen(e) {
+    e.target.style.backgroundColor = currentPenColor
+}
+
+
+window.onload = () => {
+    generateGrid(DEFAULT_SIZE)
+  }
