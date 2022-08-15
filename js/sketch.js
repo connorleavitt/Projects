1ea3741c164b6f622825a15a4dd34caddf562b87
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-08-15 13:37:02 -0700

    Add drag draw, color options

--- js/sketch.js
+++ js/sketch.js
@@ -5,10 +5,15 @@ const DEFAULT_SIZE = 16;
 let currentPenColor = DEFAULT_PEN_COLOR;
 let currentMode = DEFAULT_MODE;
 let currentSize = DEFAULT_SIZE;
-let currentlyActive = false;
+let gridLines = true;
+let clickActive = false;
 
 function setCurrentPenColor(newPenColor) {
     currentPenColor = newPenColor;
+  }
+  
+function setCurrentMode(newMode) {
+      currentMode = newMode;
   }
 
 function setCurrentSize(newSize) {
@@ -18,25 +23,37 @@ function setCurrentSize(newSize) {
 
 
 const container = document.querySelector('#container');
+const colorButton = document.getElementById('colorButton')
+const rainbowButton = document.getElementById('rainbowButton')
+const eraserButton = document.getElementById('eraserButton')
+const gridLineButton = document.getElementById('gridLineButton')
 const userGridSize = document.querySelector('#grid-size');
 const clearGridBtn = document.querySelector('#clear-grid');
 const penColorChoice = document.getElementById("penColor");
 const clearButton = document.getElementById("clear-grid");
-const cell = document.getElementsByClassName("grid-item");
 
+
+container.onmousedown = () => (clickActive = true);
+container.onmouseup = () => (clickActive = false);
+container.onmouseleave = () => (clickActive = false);
 userGridSize.onmousemove = (e) => gridSizeValue(e.target.value)
 userGridSize.onchange = (e) => updateSize(e.target.value)
 penColorChoice.oninput = (e) => setCurrentPenColor(e.target.value)
 clearButton.onclick = () => resetGrid()
+colorButton.onclick = () => setCurrentMode('color')
+rainbowButton.onclick = () => setCurrentMode('rainbow')
+eraserButton.onclick = () => setCurrentMode('eraser')
+gridLineButton.onclick = () => gridLineToggle()
 
 function gridSizeValue(value) {
-    document.getElementById("gridSizeValue").innerHTML = `${value} x ${value}`;
+    document.getElementById("gridSizeValue").innerHTML = `Grid Size: ${value} x ${value}`;
 }
 
 function updateSize(value) {
     setCurrentSize(value)
     gridSizeValue(value)
     resetGrid(value)
+    gridLines = true;
 }
 
 function resetGrid() {
@@ -57,24 +74,54 @@ function generateGrid(size) {
     for (let i = 0; i < (size * size); i++) {
         let newGridDiv = document.createElement('div');
         newGridDiv.classList.add("grid-item");
+        newGridDiv.classList.add("grid-lines");
+        newGridDiv.addEventListener('mousedown', activatePen)
+        newGridDiv.addEventListener('mouseover', activatePen)
         container.append(newGridDiv);
     }
 }
 
-function togglePen() {
-    if (!currentlyActive) {
-        cell.forEach( e => {
-            e.addEventListener('mouseleave', activatePen)
-        })
+function activatePen(e) {
+    if (!clickActive && e.type === 'mouseover') return
+    else if (currentMode === 'color') {
+        e.target.style.backgroundColor = currentPenColor
+    } else if (currentMode === 'rainbow') {
+        let randomR = Math.floor(Math.random() * 256)
+        let randomG = Math.floor(Math.random() * 256)
+        let randomB = Math.floor(Math.random() * 256)
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === 'eraser') {
+        e.target.style.backgroundColor = 'white'
     }
 }
 
-container.addEventListener('mousedown', () => togglePen());
+function gridLineToggle () {
+    if (gridLines) {
+        let div = document.querySelectorAll(".grid-item")
+        for (let i = 0; i < div.length; i++) {
+            div[i].classList.remove("grid-lines");
+        }
+        gridLines = false;
+    } else {
+        let div = document.querySelectorAll(".grid-item")
+        for (let i = 0; i < div.length; i++) {
+            div[i].classList.add("grid-lines");
+        }
+        gridLines = true;   
+    }
+  }
 
-function activatePen(e) {
-    e.target.style.backgroundColor = currentPenColor
-}
 
+
+// function buttonSettings(newMode) {
+//     if (currentMode === 'color')
+//     if (currentMode === 'color')
+//     if (currentMode === 'color')
+
+//     if (currentMode === 'color')
+//     if (currentMode === 'color')
+//     if (currentMode === 'color')
+// }
 
 window.onload = () => {
     generateGrid(DEFAULT_SIZE)
