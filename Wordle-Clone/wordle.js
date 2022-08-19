139bba0bc20fe7d919e83ebb053f78379ba38d4d
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-08-19 12:01:57 -0700

    Add Wordle v1 - need to fix color coding on dup letters

--- Wordle-Clone/wordle.js
+++ Wordle-Clone/wordle.js
@@ -15290,8 +15290,11 @@ const dictionary = [
     "rural",
     "shave"
   ]
-  
+
 const WORD_LENGTH = 5;
+const FLIP_ANIMATION_DURATION = 500;
+const DANCE_ANIMATION_DURATION = 500;
+const keyboard = document.querySelector('[data-keyboard]')
 const alertContainer = document.querySelector('[data-alert-container]')
 const guessGrid = document.querySelector('[data-guess-grid]')
 const offsetFromDate = new Date(2022, 0, 1)
@@ -15299,38 +15302,36 @@ const msOffset = Date.now() - offsetFromDate
 const dayOffset = msOffset / 1000 / 60 / 60 / 24
 const targetWord = targetWords[Math.floor(dayOffset)]
 
-startInteraction ()
+startInteraction()
 
-function startInteraction () {
+function startInteraction() {
     document.addEventListener('click', handleMouseClick)
     document.addEventListener('keydown', handleKeyPress)
 }
 
-function stopInteraction () {
+function stopInteraction() {
     document.removeEventListener('click', handleMouseClick)
     document.removeEventListener('keydown', handleKeyPress)
 }
 
 function handleMouseClick(e) {
-    if (e.target.matches(["data-key"])) {
-        pressKey(e.target.dataset.key)
-        return
-    }
+  if (e.target.matches("[data-key]")) {
+    pressKey(e.target.dataset.key)
+    return
+  }
 
-    if (e.target.matches(['data-enter'])) {
-        submitGuess()
-        return
-    }
+  if (e.target.matches("[data-enter]")) {
+    submitGuess()
+    return
+  }
 
-    if (e.target.matches(["data-delete"])) {
-        deleteKey()
-        return
-    }
-
+  if (e.target.matches("[data-delete]")) {
+    deleteKey()
+    return
+  }
 }
 
 function handleKeyPress(e) {
-    console.log(e);
     if (e.key === "Enter") {
         submitGuess()
         return
@@ -15373,6 +15374,49 @@ function submitGuess() {
         shakeTiles(activeTiles)
         return
     }
+
+    const guess = activeTiles.reduce((word, tile) => {
+      return word + tile.dataset.letter
+    }, '')
+    
+    if (!dictionary.includes(guess)) {
+      showAlert("Not in word list")
+      shakeTiles(activeTiles)
+      return
+    }
+
+    stopInteraction()
+    activeTiles.forEach((...params) => flipTile(...params, guess))
+
+}
+
+function flipTile(tile, index, array, guess) {
+  const letter = tile.dataset.letter
+  const key = keyboard.querySelector(`[data-key="${letter}"i]`)
+  setTimeout(() => {
+    tile.classList.add("flip")
+  }, (index * FLIP_ANIMATION_DURATION) / 2)
+
+  tile.addEventListener("transitionend", () => {
+    tile.classList.remove("flip")
+    if(targetWord[index] === letter) {
+      tile.dataset.state = "correct"
+      key.classList.add("correct")
+    } else if (targetWord.includes(letter)) {
+      tile.dataset.state = "wrong-location"
+      key.classList.add("wrong-location")
+    } else {
+      tile.dataset.state = "wrong"
+      key.classList.add("wrong")
+    }
+
+    if (index === array.length - 1) {
+      tile.addEventListener("transitionend", () => {
+        startInteraction()
+        checkWinLose(guess, array)
+      }, {once: true})
+    }
+  }, {once: true})
 }
 
 function getActiveTiles() {
@@ -15404,4 +15448,35 @@ function shakeTiles(tiles) {
         {once: true}
       )
     })
+}
+
+
+function checkWinLose(guess, tiles) {
+  if (guess === targetWord) {
+    showAlert("You Win", 5000)
+    danceTiles(tiles)
+    stopInteraction()
+    return
+  }
+
+const remainingTiles = guessGrid.querySelectorAll(":not([data-letter])")
+if (remainingTiles.length === 0) {
+  showAlert(targetWord.toUpperCase(), null)
+  stopInteraction()
+}
+}
+
+function danceTiles(tiles) {
+  tiles.forEach((tile, index) => {
+    setTimeout(() => {
+      tile.classList.add("dance")
+      tile.addEventListener(
+        "animationend", 
+        () => {
+          tile.classList.remove("dance")
+        }, 
+        {once: true}
+      )
+    }, index * DANCE_ANIMATION_DURATION / 5)
+  })
 }
