7112eaec8ed0c1abb918af569fb18b66a1022353
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-10-06 13:19:44 -0700

    Update CSS for product cards

--- Shopping_Cart/shopping_cart/src/pages/Products.js
+++ Shopping_Cart/shopping_cart/src/pages/Products.js
@@ -6,7 +6,18 @@ export default function Products() {
     <div className="main--products">
       <Category />
       <section className="products-display--container">
+        <ProductCard product="aglaonema" />
+        <ProductCard product="aloe" />
+        <ProductCard product="asplenium" />
         <ProductCard product="cactus" />
+        <ProductCard product="calathea" />
+        <ProductCard product="ficusTineke" />
+        <ProductCard product="monstera" />
+        <ProductCard product="philodendron" />
+        <ProductCard product="pilea" />
+        <ProductCard product="rubberTree" />
+        <ProductCard product="sansevieria" />
+        <ProductCard product="zzPlant" />
       </section>
     </div>
   );
