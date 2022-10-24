144654f7f902ca0a7c973274a997e674d0f07d39
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-10-24 15:27:27 -0700

    Add express form practice

--- Practice/nodejs_practice/routes/users.js
+++ Practice/nodejs_practice/routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.use(logger);
+
 router.get("/", (req, res) => {
   res.render("users");
 });
@@ -9,11 +11,22 @@ router.get("/new", (req, res) => {
   res.render("newUsers");
 });
 
+router.post("/", (req, res) => {
+  const isValid = false;
+  if (isValid) {
+    users.push({ firstName: req.body.firstName });
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log("Error");
+    res.render("newUsers", { firstName: req.body.firstName });
+  }
+});
+
 router
   .route("/:id")
   .get((req, res) => {
     console.log(req.userVariable);
-    res.send(req.userVariable);
+    res.send(`the new user is ${req.userVariable.firstName}`);
   })
   .put((req, res) => {
     res.send(`UPDATE / PUT id ${req.params.id}`);
@@ -28,5 +41,10 @@ router.param("id", (req, res, next, id) => {
   req.userVariable = users[id];
   next();
 });
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 module.exports = router;
