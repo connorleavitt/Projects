85df9e09fb61fa991237dedcfd6aa75d2031ebdb
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-10-24 14:52:35 -0700

    Add req params - practice

--- Practice/nodejs_practice/routes/users.js
+++ Practice/nodejs_practice/routes/users.js
@@ -9,4 +9,24 @@ router.get("/new", (req, res) => {
   res.render("newUsers");
 });
 
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.userVariable);
+    res.send(req.userVariable);
+  })
+  .put((req, res) => {
+    res.send(`UPDATE / PUT id ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`DELETE id ${req.params.id}`);
+  });
+
+const users = [{ name: "Connor" }, { name: "Zach" }];
+
+router.param("id", (req, res, next, id) => {
+  req.userVariable = users[id];
+  next();
+});
+
 module.exports = router;
