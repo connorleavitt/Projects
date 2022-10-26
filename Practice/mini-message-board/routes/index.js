73114629125dab52e2ac03a5dec77f2d3d23d1d8
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2022-10-26 14:19:28 -0700

    Add form and post req

--- Practice/mini-message-board/routes/index.js
+++ Practice/mini-message-board/routes/index.js
@@ -1,22 +1,59 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 const messages = [
   {
     text: "Hi there!",
     user: "Amanda",
-    added: new Date(),
+    added: new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    }),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date(),
+    added: new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    }),
   },
 ];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res) {
+  res.render("form");
+});
+
+router.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.user,
+    added: new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    }),
+  });
+  res.redirect(`/`);
 });
 
 module.exports = router;
