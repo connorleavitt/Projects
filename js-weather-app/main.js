2a677419d0ac66faa133dd0b3b1b39d974726928
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2023-01-23 13:03:35 -0800

    Create current and daily render functions

--- js-weather-app/main.js
+++ js-weather-app/main.js
@@ -1,5 +1,6 @@
 // import "./style.css";
 import { getWeather } from "./weather";
+import { ICON_MAP } from "./iconMap";
 
 getWeather(37.7633, -122.4129, Intl.DateTimeFormat().resolvedOptions().timeZone)
   .then(renderWeather)
@@ -11,6 +12,44 @@ getWeather(37.7633, -122.4129, Intl.DateTimeFormat().resolvedOptions().timeZone)
 function renderWeather({ current, daily, hourly }) {
   renderCurrentWeather(current);
   renderDailyWeather(daily);
-  renderHourlyWeather(hourly);
+  // renderHourlyWeather(hourly);
   document.body.classList.remove("blurred");
+}
+
+function setValue(selector, value, { parent = document } = {}) {
+  parent.querySelector(`[data-${selector}]`).textContent = value;
+}
+
+function getIconUrl(iconCode) {
+  return `icons/${ICON_MAP.get(iconCode)}.svg`;
+}
+
+const currentIcon = document.querySelector("[data-current-icon");
+
+function renderCurrentWeather(current) {
+  currentIcon.src = getIconUrl(current.iconCode);
+  // document.querySelector("[data-current-temp").textContent =
+  //   current.currentTemp;
+  setValue("current-temp", current.currentTemp);
+  setValue("current-high", current.highTemp);
+  setValue("current-low", current.lowTemp);
+  setValue("current-fl-high", current.feelsLikeHigh);
+  setValue("current-fl-low", current.feelsLikeLow);
+  setValue("current-wind", current.windSpeed);
+  setValue("current-precip", current.precip);
+}
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" });
+const dailySection = document.querySelector("[data-day-section]");
+const dayCardTemplate = document.getElementById("day-card-template");
+
+function renderDailyWeather(daily) {
+  dailySection.innerHTML = "";
+
+  daily.forEach((day) => {
+    const element = dayCardTemplate.content.cloneNode(true);
+    setValue("temp", day.maxTemp, { parent: element });
+    setValue("date", DAY_FORMATTER.format(day.timestamp), { parent: element });
+    element.querySelector("[data-icon").src = getIconUrl(day.iconCode);
+    dailySection.append(element);
+  });
 }
