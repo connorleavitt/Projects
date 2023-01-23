f8e247eae4eca210fa642f44ea1d28bf005ede56
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2023-01-23 12:21:54 -0800

    Create parse of current weather

--- js-weather-app/main.js
+++ js-weather-app/main.js
@@ -1,1 +1,10 @@
 // import "./style.css";
+import { getWeather } from "./weather";
+
+getWeather(
+  37.7633,
+  -122.4129,
+  Intl.DateTimeFormat().resolvedOptions().timeZone
+).then((data) => {
+  console.log(data);
+});
