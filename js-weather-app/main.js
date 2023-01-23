99eb78fdbdeb7a499b4fe97d16d3a390cf3285ab
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2023-01-23 12:41:27 -0800

    Create daily and hour functions

--- js-weather-app/main.js
+++ js-weather-app/main.js
@@ -1,10 +1,16 @@
 // import "./style.css";
 import { getWeather } from "./weather";
 
-getWeather(
-  37.7633,
-  -122.4129,
-  Intl.DateTimeFormat().resolvedOptions().timeZone
-).then((data) => {
-  console.log(data);
-});
+getWeather(37.7633, -122.4129, Intl.DateTimeFormat().resolvedOptions().timeZone)
+  .then(renderWeather)
+  .catch((e) => {
+    console.error(e);
+    alert("Problem getting weather data!");
+  });
+
+function renderWeather({ current, daily, hourly }) {
+  renderCurrentWeather(current);
+  renderDailyWeather(daily);
+  renderHourlyWeather(hourly);
+  document.body.classList.remove("blurred");
+}
