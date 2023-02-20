c7fecd22b2e61ebd10098641bd32ee5f614635cb
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2023-02-20 14:07:47 -0800

    tuple practice

--- Practice/typescript/dist/index.js
+++ Practice/typescript/dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
 let id = 10;
-console.log(id);
+let firstName = "Connor";
+let isGinger = true;
+let x = "Hello";
+// x = true;
+let age;
+// age = 30;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, "Hello"];
+// Tuple
+let person = [1, "test", false];
--- Practice/typescript/src/index.ts
+++ Practice/typescript/src/index.ts
@@ -1,3 +1,16 @@
 let id: number = 10;
+let firstName: string = "Connor";
+let isGinger: Boolean = true;
 
-console.log(id);
+let x: any = "Hello";
+// x = true;
+
+let age: number;
+// age = 30;
+
+let ids: number[] = [1, 2, 3, 4, 5];
+
+let arr: any[] = [1, true, "Hello"];
+
+// Tuple
+let person: [number, string, boolean] = [1, "test", false];
