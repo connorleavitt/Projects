7bca401d523f15f0d1582f20313e510e86af19e4
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2023-02-20 14:55:16 -0800

    Further classes practice

--- Practice/typescript/dist/index.js
+++ Practice/typescript/dist/index.js
@@ -10,3 +10,87 @@ let ids = [1, 2, 3, 4, 5];
 let arr = [1, true, "Hello"];
 // Tuple
 let person = [1, "test", false];
+//tuple array
+let employee;
+employee = [
+    [1, "Steve"],
+    [2, "Brad"],
+    [3, "John"],
+];
+//Union
+let pid;
+// pid = 22 || pid = "22"
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+// console.log(Direction1.Up, Direction1.Down, Direction1.Left, Direction1.Right);
+// console.log(Direction2.Up, Direction2.Down, Direction2.Left, Direction2.Right);
+// Objects
+// type User = {
+//   id: number;
+//   name: string;
+// };
+// const user: User = {
+//   id: 1,
+//   name: "Jill",
+// };
+// OR
+// const user: {
+// id: number;
+// name: string;
+// } = {
+// id: 1,
+// name: "Jill",
+// };
+// Type  Assertion
+let cid = 1;
+// let customerId = <number>cid;
+let customerId = cid;
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+function log(msg) {
+    console.log(msg);
+}
+const user1 = {
+    id: 1,
+    name: "Jill",
+    secret: 1234,
+};
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+// Classes
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered with id: ${this.id}`;
+    }
+}
+const brad = new Person(1, "Brad");
+const mike = new Person(2, "Mike");
+// console.log(brad.register());
+//Sub classes
+class Employee extends Person {
+    constructor(id, name, position) {
+        super(id, name); //extending from person class
+        this.position = position;
+    }
+}
+const emp = new Employee(3, "Sean", "Manager");
+console.log(emp.register());
--- Practice/typescript/src/index.ts
+++ Practice/typescript/src/index.ts
@@ -14,3 +14,134 @@ let arr: any[] = [1, true, "Hello"];
 
 // Tuple
 let person: [number, string, boolean] = [1, "test", false];
+
+//tuple array
+let employee: [number, string][];
+
+employee = [
+  [1, "Steve"],
+  [2, "Brad"],
+  [3, "John"],
+];
+
+//Union
+let pid: string | number;
+// pid = 22 || pid = "22"
+
+// Enum
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right",
+}
+
+// console.log(Direction1.Up, Direction1.Down, Direction1.Left, Direction1.Right);
+// console.log(Direction2.Up, Direction2.Down, Direction2.Left, Direction2.Right);
+
+// Objects
+// type User = {
+//   id: number;
+//   name: string;
+// };
+// const user: User = {
+//   id: 1,
+//   name: "Jill",
+// };
+
+// OR
+// const user: {
+// id: number;
+// name: string;
+// } = {
+// id: 1,
+// name: "Jill",
+// };
+
+// Type  Assertion
+let cid: any = 1;
+// let customerId = <number>cid;
+let customerId = cid as number;
+
+// Functions
+function addNum(x: number, y: number): number {
+  return x + y;
+}
+
+function log(msg: string | number): void {
+  console.log(msg);
+}
+
+// log("hello");
+// log(12);
+// log(true);
+
+// Interface (should use for objects)
+interface UserInterface {
+  id: number;
+  name: string;
+  age?: number; //optional with "?"
+  readonly secret: number; //read only, can't assign new value
+}
+const user1: UserInterface = {
+  id: 1,
+  name: "Jill",
+  secret: 1234,
+};
+
+// user1.secret = 12345; would error out
+
+interface MathFunc {
+  (x: number, y: number): number;
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y;
+const sub: MathFunc = (x: number, y: number): number => x - y;
+
+// console.log(add(3, 4), sub(5, 2));
+interface PersonInterface {
+  id: number;
+  name: string;
+  register(): string;
+}
+
+// Classes
+class Person implements PersonInterface {
+  id: number;
+  name: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+
+  register() {
+    return `${this.name} is now registered with id: ${this.id}`;
+  }
+}
+
+const brad = new Person(1, "Brad");
+const mike = new Person(2, "Mike");
+
+// console.log(brad.register());
+
+//Sub classes
+class Employee extends Person {
+  position: string;
+
+  constructor(id: number, name: string, position: string) {
+    super(id, name); //extending from person class
+    this.position = position;
+  }
+}
+
+const emp = new Employee(3, "Sean", "Manager");
+
+console.log(emp.register());
