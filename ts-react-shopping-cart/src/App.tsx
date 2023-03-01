b0c15ec6e99dd4e558363e523ea329c506abba34
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2023-03-01 14:50:55 -0800

    Update fs and initial setup

--- ts-react-shopping-cart/src/App.tsx
+++ ts-react-shopping-cart/src/App.tsx
@@ -1,8 +1,23 @@
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { Navbar } from "./components/Navbar";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
