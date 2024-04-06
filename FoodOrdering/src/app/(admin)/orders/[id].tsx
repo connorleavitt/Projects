326b51ca8ccf7d242222340fc00fe5dceff3d76c
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2024-04-06 13:19:10 -0700

    update status

--- FoodOrdering/src/app/(admin)/orders/[id].tsx
+++ FoodOrdering/src/app/(admin)/orders/[id].tsx
@@ -1,9 +1,11 @@
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, Pressable } from "react-native";
 import { Stack, useLocalSearchParams } from "expo-router";
 import orders from "../../../../assets/data/orders";
 import OrderListItem from "../../../components/OrderListItem";
 import OrderItemListItem from "@/src/components/OrderItemListItem";
 import { FlashList } from "@shopify/flash-list";
+import Colors from "@/src/constants/Colors";
+import { OrderStatusList } from "@/src/types";
 
 const OrderDetailScreen = () => {
   const { id } = useLocalSearchParams();
@@ -17,15 +19,50 @@ const OrderDetailScreen = () => {
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: `Order #${order.id}` }} />
-      {/* <OrderListItem order={order} /> */}
+      <OrderListItem order={order} />
       <FlashList
         data={order.order_items}
         renderItem={({ item }) => <OrderItemListItem item={item} />}
         estimatedItemSize={100}
         ListHeaderComponent={
           <>
-            <OrderListItem order={order} />
+            {/* <OrderListItem order={order} /> */}
             <Text style={{ fontWeight: "bold" }}>Order Items</Text>
+          </>
+        }
+        ListFooterComponent={
+          <>
+            <Text style={{ fontWeight: "bold" }}>Status</Text>
+            <View style={{ flexDirection: "row", gap: 5 }}>
+              {OrderStatusList.map((status) => (
+                <Pressable
+                  key={status}
+                  onPress={() => console.warn("Update status")}
+                  style={{
+                    borderColor: Colors.light.tint,
+                    borderWidth: 1,
+                    padding: 10,
+                    borderRadius: 5,
+                    marginVertical: 10,
+                    flex: 1,
+                    alignItems: "center",
+                    backgroundColor:
+                      order.status === status
+                        ? Colors.light.tint
+                        : "transparent",
+                  }}
+                >
+                  <Text
+                    style={{
+                      color:
+                        order.status === status ? "white" : Colors.light.tint,
+                    }}
+                  >
+                    {status}
+                  </Text>
+                </Pressable>
+              ))}
+            </View>
           </>
         }
       />
