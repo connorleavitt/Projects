1ec1bdb951e7e92505a34cdb59723eb430f53903
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2024-04-26 20:55:51 -0700

    home screen: polls list

--- polls/src/app/index.tsx
+++ polls/src/app/index.tsx
@@ -1,20 +1,47 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { FlashList } from "@shopify/flash-list";
+import { Stack } from "expo-router";
+
+const polls = [1, 2, 3, 4, 5];
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>HIII</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          title: "Polls",
+          headerStyle: { backgroundColor: "lightblue" },
+          headerTintColor: "black",
+          headerTitleStyle: { fontWeight: "bold" },
+        }}
+      />
+      <FlashList
+        data={polls}
+        contentContainerStyle={styles.container}
+        estimatedItemSize={50}
+        renderItem={({ item }) => (
+          <View style={styles.pollContainer}>
+            <Text style={styles.pollTitle}>Placeholder poll question</Text>
+          </View>
+        )}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "gainsboro",
+    padding: 10,
+  },
+  pollContainer: {
+    backgroundColor: "lightblue",
+    padding: 10,
+    borderRadius: 10,
+  },
+  pollTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
