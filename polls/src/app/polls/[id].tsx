c744ae318c07c597cac0b0ce0a506ed8e7d306c2
connorleavitt <108369689+connorleavitt@users.noreply.github.com>
2024-04-29 15:49:48 -0700

    user vote update

--- polls/src/app/polls/[id].tsx
+++ polls/src/app/polls/[id].tsx
@@ -10,7 +10,7 @@ import {
 import React, { useEffect, useState } from "react";
 import { Redirect, Stack, useLocalSearchParams } from "expo-router";
 import { AntDesign } from "@expo/vector-icons";
-import { Poll } from "@/types/db";
+import { Poll, Vote } from "@/types/db";
 import { supabase } from "@/lib/supabase";
 import { useAuth } from "@/providers/AuthProvider";
 
@@ -18,6 +18,8 @@ export default function PollDetails() {
   const { id } = useLocalSearchParams();
   const [poll, setPoll] = useState<Poll | null>(null);
   const [selectedOption, setSelectedOption] = useState("");
+
+  const [userVote, setUserVote] = useState<Vote | null>(null);
 
   const { user } = useAuth();
 
@@ -37,16 +39,61 @@ export default function PollDetails() {
       }
       setPoll(data as Poll);
     };
+    //fetch user vote
+    const fetchUserVote = async () => {
+      if (!user) {
+        return;
+      }
+      let { data, error } = await supabase
+        .from("votes")
+        .select("*")
+        .eq("user_id", user.id)
+        .eq("poll_id", Number.parseInt(id as string))
+        .limit(1)
+        .single();
+      if (error) {
+        // console.log(error.message);
+      }
+      setUserVote(data as Vote);
+      if (data) {
+        setSelectedOption(data.option);
+      } else {
+        setSelectedOption("");
+      }
+    };
     fetchPolls();
+    fetchUserVote();
   }, []);
 
   const vote = async () => {
-    console.warn("Voted", selectedOption);
+    if (!selectedOption) {
+      Alert.alert("Select an option to vote");
+      return;
+    }
+    if (!poll?.id || !user?.id) {
+      Alert.alert("Poll or user ID is missing");
+      return;
+    }
+    const newVote: Vote = {
+      option: selectedOption,
+      poll_id: poll.id,
+      user_id: user.id,
+      id: userVote?.id || undefined,
+    };
 
     const { data, error } = await supabase
       .from("votes")
-      .insert([{ option: selectedOption, poll_id: poll.id, user_id: user.id }])
-      .select();
+      .upsert([newVote])
+      .select()
+      .single();
+    if (error) {
+      Alert.alert("Failed to vote");
+      console.log(error.message);
+      return;
+    } else {
+      setUserVote(data as Vote);
+      Alert.alert("Voted successfully");
+    }
   };
   if (!poll) {
     return (
@@ -82,6 +129,9 @@ export default function PollDetails() {
       {selectedOption && (
         <Button title="Remove vote" onPress={() => setSelectedOption("")} />
       )}
+      <View style={{ padding: 10 }}>
+        <Text>Your vote: {userVote?.option}</Text>
+      </View>
     </View>
   );
 }
